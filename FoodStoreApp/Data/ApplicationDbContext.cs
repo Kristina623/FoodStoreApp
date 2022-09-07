2a8f5f2a9146dba9609ebd75691f740a062d98a7
323 <323@323-3>
2022-09-07 10:32:48 +0300

    Добавьте файлы проекта.

---
+++ FoodStoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using FoodStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FoodStoreApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
